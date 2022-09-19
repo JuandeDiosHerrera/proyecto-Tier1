eeefb3b5251e6b4c699ba6a2a6971e8c37dad91c
JuandeDiosHerrera <jdd7sfc@gmail.com>
2022-09-19 13:07:42 +0200

    Actualización barcode

--- barcode.py
+++ barcode.py
@@ -1,5 +1,16 @@
 import cv2
+from matplotlib import pyplot as plt
+
  
 bardet = cv2.barcode_BarcodeDetector()
 img = cv2.imread("E:\\Documents\\Juan de Dios\\TFG\\foto.jpg")
 ok, decoded_info, decoded_type, corners = bardet.detectAndDecode(img)
+
+print(ok)
+print('')
+print('Decoded info:',decoded_info)
+print('')
+print('Decoded type:',decoded_type)
+print('')
+print('Corners:', corners)
+print('')
