b1779c29da0b513b9043ac44d9abebde2d9f253f
JuandeDiosHerrera <jdd7sfc@gmail.com>
2022-09-19 14:15:53 +0200

    Rectángulo que rodea al código de barras

--- barcode.py
+++ barcode.py
@@ -5,6 +5,10 @@ from matplotlib import pyplot as plt
 bardet = cv2.barcode_BarcodeDetector()
 img = cv2.imread("E:\\Documents\\Juan de Dios\\TFG\\foto.jpg")
 ok, decoded_info, decoded_type, corners = bardet.detectAndDecode(img)
+"""
+cv2.imshow('Image', img)
+cv2.waitKey()
+"""
 
 print(ok)
 print('')
@@ -14,3 +18,17 @@ print('Decoded type:',decoded_type)
 print('')
 print('Corners:', corners)
 print('')
+#print(len(corners))
+
+img_copy = img.copy()
+
+for i in range(len(corners)):  
+    for j in range(4):              
+        x = corners[i][j][0]                #Desde esquina inferior izquierda en sentido horario
+        y = corners[i][j][1]
+        #print(x,y)
+        cv2.circle(img_copy, (int(x),int(y)), 15, (255,0,0), -1)
+    
+plt.subplot(111),plt.imshow(img_copy)
+plt.title('Image'), plt.xticks([]), plt.yticks([])
+plt.show()
