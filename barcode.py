dc040fa936cca2eb28931159cc2344e09f091bf0
JuandeDiosHerrera <jdd7sfc@gmail.com>
2022-10-03 18:39:26 +0200

    Lectura de varias imágenes para "barcode.py"

--- barcode.py
+++ barcode.py
@@ -1,34 +1,48 @@
 import cv2
 from matplotlib import pyplot as plt
-
+from os import listdir
+from os.path import isfile, join
+import numpy
  
 bardet = cv2.barcode_BarcodeDetector()
-img = cv2.imread("E:\\Documents\\Juan de Dios\\TFG\\foto.jpg")
-ok, decoded_info, decoded_type, corners = bardet.detectAndDecode(img)
+
+mypath='E:\\Documents\\Juan de Dios\\TFG\\Fotos'
+onlyfiles = [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
+images = numpy.empty(len(onlyfiles), dtype=object)
+for n in range(0, len(onlyfiles)):
+    images[n] = cv2.imread( join(mypath,onlyfiles[n]) )
+    ok, decoded_info, decoded_type, corners = bardet.detectAndDecode(images[n])
+   
+    plt.subplot(111),plt.imshow(images[n])
+    plt.title('Image'), plt.xticks([]), plt.yticks([])
+    plt.show()
+
+    print(ok)    
+    print('Decoded info:',decoded_info)
+    print('Decoded type:',decoded_type)
+    print('Corners:', corners)
+    print('')
+
+    img_copy = images[n].copy()
+
+    for i in range(len(corners)):  
+        for j in range(4):              
+            x = corners[i][j][0]                #Desde esquina inferior izquierda en sentido horario
+            y = corners[i][j][1]
+            #print(x,y)
+            cv2.circle(img_copy, (int(x),int(y)), 15, (255,0,0), -1)
+   
+    plt.subplot(111),plt.imshow(img_copy)
+    plt.title('Image'), plt.xticks([]), plt.yticks([])
+    plt.show()
+
 """
 cv2.imshow('Image', img)
 cv2.waitKey()
 """
 
-print(ok)
-print('')
-print('Decoded info:',decoded_info)
-print('')
-print('Decoded type:',decoded_type)
-print('')
-print('Corners:', corners)
-print('')
+
 #print(len(corners))
 
-img_copy = img.copy()
 
-for i in range(len(corners)):  
-    for j in range(4):              
-        x = corners[i][j][0]                #Desde esquina inferior izquierda en sentido horario
-        y = corners[i][j][1]
-        #print(x,y)
-        cv2.circle(img_copy, (int(x),int(y)), 15, (255,0,0), -1)
     
-plt.subplot(111),plt.imshow(img_copy)
-plt.title('Image'), plt.xticks([]), plt.yticks([])
-plt.show()
