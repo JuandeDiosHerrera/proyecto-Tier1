8225f5419908c6471d0ddd1f2fb6477b3afe7d2c
JuandeDiosHerrera <jdd7sfc@gmail.com>
2022-10-17 10:35:27 +0200

    Update barcode.py

--- barcode.py
+++ barcode.py
@@ -1,3 +1,4 @@
+from types import NoneType
 import cv2
 from matplotlib import pyplot as plt
 from os import listdir
@@ -11,31 +12,51 @@ onlyfiles = [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
 images = numpy.empty(len(onlyfiles), dtype=object)
 for n in range(0, len(onlyfiles)):
     images[n] = cv2.imread( join(mypath,onlyfiles[n]) )
+    images[n] = cv2.cvtColor(images[n], cv2.COLOR_BGR2RGB)
+
     ok, decoded_info, decoded_type, corners = bardet.detectAndDecode(images[n])
    
+    """
     plt.subplot(111),plt.imshow(images[n])
     plt.title('Image'), plt.xticks([]), plt.yticks([])
     plt.show()
-
+    
+    # Detección, códigos decodificados, tipos de códigos y esquinas de los códigos:
     print(ok)    
     print('Decoded info:',decoded_info)
     print('Decoded type:',decoded_type)
     print('Corners:', corners)
     print('')
+    """
 
     img_copy = images[n].copy()
+    #print(corners)
+    if ok == False:
+        print('Código de barra no detectado')
 
-    for i in range(len(corners)):  
-        for j in range(4):              
-            x = corners[i][j][0]                #Desde esquina inferior izquierda en sentido horario
-            y = corners[i][j][1]
-            #print(x,y)
-            cv2.circle(img_copy, (int(x),int(y)), 15, (255,0,0), -1)
-   
-    plt.subplot(111),plt.imshow(img_copy)
-    plt.title('Image'), plt.xticks([]), plt.yticks([])
-    plt.show()
+        plt.subplot(111),plt.imshow(images[n])
+        plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+        plt.show()
 
+    else:
+        for i in range(len(corners)):  
+            for j in range(4):              
+                x = corners[i][j][0]                #Desde esquina inferior izquierda en sentido horario
+                y = corners[i][j][1]
+                #print(x,y)
+                cv2.circle(img_copy, (int(x),int(y)), 15, (255,0,0), -1)
+        
+        plt.subplot(121),plt.imshow(images[n])
+        plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+
+        plt.subplot(122),plt.imshow(img_copy)
+        plt.title('Barcode detection'), plt.xticks([]), plt.yticks([])
+        plt.show()
+        """
+        plt.subplot(111),plt.imshow(img_copy)
+        plt.title('Image'), plt.xticks([]), plt.yticks([])
+        plt.show()
+        """
 """
 cv2.imshow('Image', img)
 cv2.waitKey()
