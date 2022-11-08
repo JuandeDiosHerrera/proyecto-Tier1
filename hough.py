594945438ef3dac2ed0eb282d6b37ae79f1c8335
JuandeDiosHerrera <jdd7sfc@gmail.com>
2022-11-08 14:01:19 +0100

    Bucle umbral en "hough.py"

--- hough.py
+++ hough.py
@@ -29,8 +29,17 @@ def funcion():
 		closed = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel)
 
 		#Búsqueda de líneas horizontales: en el rango [85º,95º]
-		lines = cv2.HoughLines(closed,rho=1,theta=numpy.pi/180,threshold=2000,srn=0,stn=0,min_theta=numpy.pi/2-0.08726,max_theta=numpy.pi/2+0.08726)
-		print(len(lines))
+		lineas_detectadas = 2000
+		umbral = 100
+		while(lineas_detectadas>15):
+			lines = cv2.HoughLines(edges,rho=1,theta=numpy.pi/180,threshold=umbral,srn=0,stn=0,min_theta=numpy.pi/2-0.08726,max_theta=numpy.pi/2+0.08726)
+			# print(lines)			
+			lineas_detectadas = len(lines)
+			print(len(lines),umbral)			
+			umbral += 100
+
+		print('')		
+
 		linesP = cv2.HoughLinesP(edges,rho=1,theta=numpy.pi/180,threshold=300,minLineLength=100,maxLineGap=5)
 		img_copy1 = images[n].copy()
 		img_copy2 = images[n].copy()	
@@ -39,7 +48,7 @@ def funcion():
 		#print(lines1)
 		#print(lines1[0][2][0])			#Campo 0: nada, campo 1: el par (rho, theta), campo 2: rho [0] y theta [1]
 		if lines is not None:
-			for i in range(0, len(lines)):
+			for i in range(len(lines)):
 				#print(i)
 				rho = lines[i][0][0]
 				theta = lines[i][0][1]
@@ -50,7 +59,7 @@ def funcion():
 				y0 = b * rho				# y = rho * sin(theta)
 				pt1 = (int(x0 + math.sqrt(height**2+width**2)*(-b)), int(y0 + math.sqrt(height**2+width**2)*(a)))			#Tamaño imagen: 3000(alto) x 4000(ancho) 
 				pt2 = (int(x0 - math.sqrt(height**2+width**2)*(-b)), int(y0 - math.sqrt(height**2+width**2)*(a)))
-				print(pt1,pt2)
+				#print(pt1,pt2)
 				cv2.line(img_copy1, pt1, pt2, (255,0,0), 10, cv2.LINE_AA)
 
 		# Draw the lines
@@ -69,8 +78,8 @@ def funcion():
 		plt.subplot(223),plt.imshow(img_copy1)
 		plt.title('Líneas detectadas '), plt.xticks([]), plt.yticks([])
 		
-		plt.subplot(224),plt.imshow(closed,cmap = 'gray')
-		plt.title('Rellenar bordes'), plt.xticks([]), plt.yticks([])		
+		plt.subplot(224),plt.imshow(img_copy2,cmap = 'gray')
+		plt.title('Porbabilístico'), plt.xticks([]), plt.yticks([])		
 		plt.show()
 		
 		#############################################PROBAR cv2.ADAPTIVE_THRESH_GAUSSIAN_C#########################################################
